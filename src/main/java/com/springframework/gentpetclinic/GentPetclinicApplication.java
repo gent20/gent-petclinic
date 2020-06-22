8a35b486e3cefc1899fcbf73eba364d2df2a4665
gent20 <123qweasdbnM>
2020-06-22 19:05:44 +0200

    Initial Project Add

---
+++ src/main/java/com/springframework/gentpetclinic/GentPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.gentpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GentPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GentPetclinicApplication.class, args);
+    }
+
+}
